6ae15213ee02f240e5382bf6d3ef0f08e5000b0c
euberdeveloper <euberdeveloper@gmail.com>
2020-12-24 21:15:27 +0100

    refactor(colour.ts): improve code readability

--- source/utils/colour.ts
+++ source/utils/colour.ts
@@ -24,15 +24,17 @@ const COLOURS = [
 export function colour(colour: string): chalk.Chalk {
     if (/^#([a-f0-9]{3}){1,2}$/i.test(colour)) {
         return chalk.hex(colour);
-    } else {
-        const regexpResult = /^\((?<red>\d{1,3}),(?<green>\d{1,3}),(?<blue>\d{1,3})\)$/.exec(colour)?.groups;
-        if (regexpResult) {
-            const { red, green, blue } = regexpResult;
-            return chalk.rgb(+red, +green, +blue);
-        } else if (COLOURS.includes(colour)) {
-            return chalk[colour];
-        } else {
-            return chalk.keyword(colour);
-        }
     }
+
+    const regexpResult = /^\((?<red>\d{1,3}),(?<green>\d{1,3}),(?<blue>\d{1,3})\)$/.exec(colour)?.groups;
+    if (regexpResult) {
+        const { red, green, blue } = regexpResult;
+        return chalk.rgb(+red, +green, +blue);
+    }
+
+    if (COLOURS.includes(colour)) {
+        return chalk[colour];
+    }
+
+    return chalk.keyword(colour);
 }
