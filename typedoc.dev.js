51857fd4689ef69fc83fe7d44b2b9f2e030fbf35
euberdeveloper <euberdeveloper@gmail.com>
2020-12-30 19:21:30 +0100

    docs: remove ltypedoc

--- typedoc.dev.js
+++ typedoc.dev.js
@@ -1,8 +1,8 @@
 module.exports = {
-    mode: 'modules',
+    entryPoints: [
+        './source'
+    ],
     name: 'euberlog - DEV',
-    entryPoint: 'source/index.ts',
-    includeVersion: true,
     tsconfig: 'source/tsconfig.json',
     gaID: process.env.GA_TOKEN,
     out: './docs/documentation/html-dev'
