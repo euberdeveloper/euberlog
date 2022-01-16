b8a9804e4f18f98f08256f376937f23c46c28482
euberdeveloper <euberdeveloper@gmail.com>
2022-01-16 14:24:35 +0100

    test(logger): fix by adding test constructor
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- test/suites/logger/logger.test.ts
+++ test/suites/logger/logger.test.ts
@@ -1,8 +1,10 @@
+import testConstructor from './constructor/constructor.test';
 import testLogs from './logs/logs.test';
 import testSetOptions from './setOptions/setOptions.test';
 
 export default function testLogger(): void {
     describe('Test @/utils/logger.ts', function () {
+        testConstructor();
         testLogs();
         testSetOptions();
     });
