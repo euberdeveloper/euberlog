36c68d863becca9f03bf1563d294a9f61a8e15d6
euberdeveloper <euberdeveloper@gmail.com>
2022-01-16 14:36:11 +0100

    docs(index.ts): add doc comment for logger instance
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- source/index.ts
+++ source/index.ts
@@ -2,5 +2,6 @@ export * from '@/types';
 export * from '@/utils/logger';
 
 import { Logger } from '@/utils/logger';
+/** A default exported instance of the [Logger] class, created with the default options */
 const logger = new Logger();
 export default logger;
