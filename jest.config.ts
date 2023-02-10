d61a0b529f11173775379ed4ad9a3824649a13dc
euberdeveloper <euberdeveloper@gmail.com>
2023-02-10 23:26:00 +0100

    test(jest.config.ts): pass to transform
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- jest.config.ts
+++ jest.config.ts
@@ -3,28 +3,29 @@ import { pathsToModuleNameMapper } from 'ts-jest';
 import tsconfigJson from './tsconfig.json';
 
 function manageKey(key: string): string {
-   return key.includes('(.*)') ? key.slice(0, -1) + '\\.js$' : key;
+    return key.includes('(.*)') ? key.slice(0, -1) + '\\.js$' : key;
 }
 function manageMapper(mapper: Record<string, string>): Record<string, string> {
-   const newMapper: Record<string, string> = {};
-   for (const key in mapper) {
-      newMapper[manageKey(key)] = mapper[key];
-   }
-   newMapper['^(.*).js$'] = '$1';
-   return newMapper;
+    const newMapper: Record<string, string> = {};
+    for (const key in mapper) {
+        newMapper[manageKey(key)] = mapper[key];
+    }
+    newMapper['^(.*).js$'] = '$1';
+    return newMapper;
 }
 
 const config: Config.InitialOptions = {
     preset: 'ts-jest/presets/default-esm',
     testEnvironment: 'node',
     verbose: true,
-    globals: {
-        'ts-jest': {
+    transform: {
+        '^.+\\.tsx?$': ['ts-jest', {
             tsconfig: './tsconfig.json',
-            useESM: true
-        }
+            useEsm: true
+        }]
     },
     coverageProvider: 'v8',
+    collectCoverageFrom: ["./source/**/*.ts"],
     moduleNameMapper: manageMapper(pathsToModuleNameMapper(tsconfigJson.compilerOptions.paths, { prefix: '<rootDir>/' }) as Record<string, string>),
     transformIgnorePatterns: ['<rootDir>/node_modules/']
 };
